5c79b23d13aa38527e67fcf67ad349694beeac2e
s23301955 <34060155+s23301955@users.noreply.github.com>
2021-04-07 10:49:29 +0800

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,78 @@
+function over_s1() {
+        document.getElementById('s1').style.visibility = 'visible';
+    }
+    function out_s1() {
+        document.getElementById('s1').style.visibility = 'hidden'
+    }
+    function over_s2() {
+        document.getElementById('s2').style.visibility = 'visible'
+    }
+    function out_s2() {
+        document.getElementById('s2').style.visibility = 'hidden'
+    }
+    function over_s3() {
+        document.getElementById('s3').style.visibility = 'visible'
+    }
+    function out_s3() {
+        document.getElementById('s3').style.visibility = 'hidden'
+    }
+    function over_s4() {
+        document.getElementById('s4').style.visibility = 'visible'
+    }
+    function out_s4() {
+        document.getElementById('s4').style.visibility = 'hidden'
+    }
+    function over_s5() {
+        document.getElementById('s5').style.visibility = 'visible'
+    }
+    function out_s5() {
+        document.getElementById('s5').style.visibility = 'hidden'
+    }
+    function over_s6() {
+        document.getElementById('s6').style.visibility = 'visible'
+    }
+    function out_s6() {
+        document.getElementById('s6').style.visibility = 'hidden'
+    }
+    function over_s7() {
+        document.getElementById('s7').style.visibility = 'visible'
+    }
+    function out_s7() {
+        document.getElementById('s7').style.visibility = 'hidden'
+    }
+    function over_s8() {
+        document.getElementById('s8').style.visibility = 'visible'
+    }
+    function out_s8() {
+        document.getElementById('s8').style.visibility = 'hidden'
+    }
+    function over_s9() {
+        document.getElementById('s9').style.visibility = 'visible'
+    }
+    function out_s9() {
+        document.getElementById('s9').style.visibility = 'hidden'
+    }
+    function over_s10() {
+        document.getElementById('s10').style.visibility = 'visible'
+    }
+    function out_s10() {
+        document.getElementById('s10').style.visibility = 'hidden'
+    }
+    function over_s11() {
+        document.getElementById('s11').style.visibility = 'visible'
+    }
+    function out_s11() {
+        document.getElementById('s11').style.visibility = 'hidden'
+    }
+    function over_s12() {
+        document.getElementById('s12').style.visibility = 'visible'
+    }
+    function out_s12() {
+        document.getElementById('s12').style.visibility = 'hidden'
+    }
+    function over_s13() {
+        document.getElementById('s13').style.visibility = 'visible'
+    }
+    function out_s13() {
+        document.getElementById('s13').style.visibility = 'hidden'
+    }
